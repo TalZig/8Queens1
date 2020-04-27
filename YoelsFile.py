bde8d1c06371650bb4b67ed62220297853d2dcdf
TalZig <50695642+TalZig@users.noreply.github.com>
2020-04-27 22:00:36 +0300

    now the answer is correct

--- YoelsFile.py
+++ YoelsFile.py
@@ -10,21 +10,22 @@ def initialPopulation():
         for i in range(8):
             chrome.append(randrange(8))
 
-        pop.append(chrome);
+        pop.append(chrome)
 
     return pop
 
 def fitnessFunction(list):
     grade = 49
     slant = 0
-    for i in range(0,7):
-        for j in range(i + 1,7):
+    for i in range(0,8):
+        for j in range(i + 1,8):
             slant = j - i
-            if(list[i] == list[j]):
+            if list[i] == list[j]:
                 grade = grade - 2
             else:
-                if(list[i] + slant == list[j] or list[i] - slant == list[j]):
+                if list[i] + slant == list[j] or list[i] - slant == list[j]:
                     grade = grade - 2
+    print(grade)
     return grade
 
 
@@ -36,9 +37,9 @@ def crossOver(list1, list2):
     randNum = random.randint(0,10)
     if(randNum <2):
         return list1 ,list2
-    childrenList1 =[7]
-    childrenList2 = [7]
-    for j in range(0,7):
+    childrenList1 =[]
+    childrenList2 = []
+    for j in range(0,8):
         i = random.randint(0, 1)
         if(i == 1):
             childrenList1.append(list1[j])
@@ -47,11 +48,11 @@ def crossOver(list1, list2):
             childrenList1.append(list2[j])
             childrenList2.append(list1[j])
 
-    for i in range (0,7):
-        randNum = random.randint(0, 500)
+    for i in range (0,8):
+        randNum = random.randint(0, 100)
         if(randNum == 0):
             childrenList1 = mutation(childrenList1,i)
-        randNum = random.randint(0,500)
+        randNum = random.randint(0,100)
         if(randNum == 0):
             childrenList2 = mutation(childrenList2,i)
     return childrenList1, childrenList2
@@ -60,29 +61,31 @@ def createNextGen(currGen, grades):
     nextGen = []
 
     #elitism
-    highest = max(currGen)
-    temp = [i for i,j in enumerate(currGen) if j == highest]
-    nextGen.append(currGen[temp[0]])
-    currGen.pop(temp[0])
-    grades.pop(temp[0])
+    highest = grades.index(max(grades))
+    #temp = [i for i,j in enumerate(currGen) if j == highest]
+    temp = currGen[highest]
+    nextGen.append(temp)
+    currGen.pop(highest)
+    grades.pop(highest)
 
-    highest = max(currGen)
-    temp = [i for i, j in enumerate(currGen) if j == highest]
-    nextGen.append(currGen[temp[0]])
-    currGen.pop(temp[0])
-    grades.pop(temp[0])
+    highest = grades.index(max(grades))
+    #temp = [i for i,j in enumerate(currGen) if j == highest]
+    temp = currGen[highest]
+    nextGen.append(temp)
+    currGen.pop(highest)
+    grades.pop(highest)
 
-    lowest = min(currGen)
-    temp = [i for i, j in enumerate(currGen) if j == lowest]
-    nextGen.append(currGen[temp[0]])
-    currGen.pop(temp[0])
-    grades.pop(temp[0])
+    lowest = grades.index(min(grades))
+    temp = currGen[lowest]
+    #nextGen.append(temp)
+    currGen.pop(lowest)
+    grades.pop(lowest)
 
-    lowest = min(currGen)
-    temp = [i for i, j in enumerate(currGen) if j == lowest]
-    nextGen.append(currGen[temp[0]])
-    currGen.pop(temp[0])
-    grades.pop(temp[0])
+    lowest = grades.index(min(grades))
+    temp = currGen[lowest]
+    #nextGen.append(temp)
+    currGen.pop(lowest)
+    grades.pop(lowest)
 
 
     #do 100 times: select 2, crossover, mutation, add to new gen
@@ -115,18 +118,18 @@ def main():
 
     createNextGen(currGen, grades)
 
-    while (not grades.count(49)):
+    while 49 not in grades:
         currGen = createNextGen(currGen, grades)
         grades = []
         for l in currGen:
             grades.append(fitnessFunction(l))
         gen = gen + 1
-        print(gen)
+        #print(gen)
 
     index = grades.index(49)
 
-    print("Solution found after " + gen + " generations.")
-    print("The solution is: " + currGen[index])
+    print("Solution found after " + str(gen) + " generations.")
+    print("The solution is: " + str(currGen[index]))
 
 
 
